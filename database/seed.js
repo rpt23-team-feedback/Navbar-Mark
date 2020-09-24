0cc6ae7cadf9dfc1d9ee4a98500bf2b3a4ead018
Mark <mark.scrudder@gmail.com>
2020-09-24 11:43:05 -0700

    Initial setup for seed script.

--- database/seed.js
+++ database/seed.js
@@ -1,9 +1,19 @@
 const db = require('index.js');
 
-const timerSeed = seedGen();
+const seed = () => {
+  return db.bulkCreate(seedGen());
+}
 
 seedGen = function () {
+  var bundleId, countDown;
+  var timerData = [];
   for (let i = 1; i < 100; i++) {
-    //
+    bundleId = i;
+    countDown = Math.floor (Math.random * 13) + 1;  // countdown in days, may adjust to be more granular later
+    timerData.push ({
+      bundleId : bundleId,
+      countDown : countDown
+    })
   }
+  return timerData;
 };
