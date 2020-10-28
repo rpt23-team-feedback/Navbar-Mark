713930c81c842817f895534905acc4d3c3f1f142
Mark <mark.scrudder@gmail.com>
2020-10-27 20:05:12 -0700

    Added formatting for default service with no API calls.

--- server/index.js
+++ server/index.js
@@ -10,22 +10,22 @@ app.use(express.static(__dirname + '/../dist'));
 
 const PORT = 3102;
 
-app.get('/countdown/:bundleId', (req, res) => {
-  const dataString = req.params;
-  return db.countdownRequest(dataString)
-  .then(countdownData => {
-    if (countdownData) {
-      return countdownData;
-    } else {
-      res.status(404).send('no such bundleId');
-    }
-  })
-  .then(countdownData => {
-    res.send(JSON.stringify(countdownData));
-  })
-  .catch(err => {
-    res.status(500).send('wait and try again', err);
-  })
-});
+// app.get('/countdown/:bundleId', (req, res) => {
+//   const dataString = req.params;
+//   return db.countdownRequest(dataString)
+//   .then(countdownData => {
+//     if (countdownData) {
+//       return countdownData;
+//     } else {
+//       res.status(404).send('no such bundleId');
+//     }
+//   })
+//   .then(countdownData => {
+//     res.send(JSON.stringify(countdownData));
+//   })
+//   .catch(err => {
+//     res.status(500).send('wait and try again', err);
+//   })
+// });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import $ from 'jquery';
+import './index.css';
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      countdown: 14,
+      bundleId: 1,
+      countdown: 1400,
+      tierCosts: {
+        tier1: 1,
+        tier2: 3,
+        tier3: 10,
+      }
     }
+    // this.getCountdownData = this.getCountdownData.bind(this);
   }
+
+  componentDidMount() {
+    // this.getCountdownData();
+  }
+
+  // getCountdownData() {
+  //   let bundleId = this.state.bundleId;
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: '/countdown/' + bundleId,
+  //     datatype: 'json',
+  //     success: (data) => {
+  //       console.log('data received', data);
+  //       this.setState({
+  //         countdown: data.countDown,
+  //       })
+  //     },
+  //     error: (err) => {
+  //       console.log('err', err);
+  //     }
+  //   })
+  // }
 
   render() {
     return (
-      <div>
-        WIP
+      <div id="Navbar" className="moving-navbar">
+        <div id="bell" className="bell moving-navbar-button">
+          B
+        </div>
+        <div className="countdown-container moving-navbar-button">
+          <div id="countdown" className="">
+            <div className="days blocky-countdown">
+              <div className="countdown-number">
+                30
+              </div>
+              <div className="countdown-word">
+                days
+              </div>
+            </div>
+            <div className="hours blocky-countdown">
+              <div className="countdown-number">
+                23
+              </div>
+              <div className="countdown-word">
+                hours
+              </div>
+            </div>
+            <div className="minutes blocky-countdown">
+              <div className="countdown-number">
+                59
+              </div>
+              <div className="countdown-word">
+                min
+              </div>
+            </div>
+            <div className="seconds blocky-countdown">
+              <div className="countdown-number">
+                59
+              </div>
+              <div className="countdown-word">
+                sec
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="tier-cost tier1" className="cost moving-navbar-button no-style">Pay $1.00+</div>
+        <div id="tier-cost tier2" className="cost moving-navbar-button no-style">Pay $3.00+</div>
+        <div id="tier-cost tier3" className="cost moving-navbar-button no-style">Pay $10.00+</div>
+        <div id="charity" className="moving-navbar-button no-style" >Charity</div>
+        <div id="stats" className="moving-navbar-button no-style">Stats</div>
+        <div id="pay-button" className="purchase-button button-green">Pay What You Want!</div>
       </div>
     )
   }
